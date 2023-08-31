02131068a3add46151ec24eb983b32b00d809b14
Sachin soni <107230805+sachin0612@users.noreply.github.com>
2023-08-31 09:37:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,4 +48,4 @@ def predict_datapoint():
     return render_template('experiment.html',results=results[0])
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0",debug=True,port=5000)
+    app.run(host="0.0.0.0",debug=True,port=8080)
